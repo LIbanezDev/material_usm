c759e71a6b4c6b80cc653fcbe0df10b753976a89
lucas <lucas.vergara@usm.cl>
2020-08-29 18:04:35 -0400

    Agregar configuracion inicial para utilizar Next JS junto a Material UI - Agregar NavBar

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,40 @@
-import React from "react";
-import 'fontsource-roboto';
-import Head from "next/head";
-import {Provider} from "react-redux";
-import store from "../redux/store";
-import withRedux from "next-redux-wrapper";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import {ThemeProvider} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/components/theme';
+import NavBar from "../src/components/NavBar";
 
-const App = ({Component, pageProps}) => {
+export default function MyApp(props) {
+
+    const {Component, pageProps} = props;
+
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
     return (
-        <>
+        <React.Fragment>
             <Head>
-                <meta
-                    name="viewport"
-                    content="minimum-scale=1, initial-scale=1, width=device-width"
-                />
+                <title>Material Universitario</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
             </Head>
-            <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline/>
+                <NavBar/>
                 <Component {...pageProps} />
-            </Provider>
-        </>
-    )
+            </ThemeProvider>
+        </React.Fragment>
+    );
 }
 
-export async function getStaticProps({Component, ctx}) {
-    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
-    //Anything returned here can be access by the client
-    return {pageProps: pageProps};
-}
-
-const makeStore = () => store;
-
-export default withRedux(makeStore)(App)
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    pageProps: PropTypes.object.isRequired,
+};
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import {AppBar, Toolbar, Typography, Button, IconButton} from '@material-ui/core';
+import Link from "next/link";
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+
+export default function NavBar() {
+
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    Material Universitario
+                </Typography>
+                <Link href="/">
+                    <Button color="inherit"> Index </Button>
+                </Link>
+                <Link href="/files">
+                    <Button color="inherit"> Files </Button>
+                </Link>
+            </Toolbar>
+        </AppBar>
+    );
+}
