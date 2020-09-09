72c4b232999b07c48bce4c6244816c17ba0668a9
lucas <lucas.vergara@usm.cl>
2020-09-09 17:31:10 -0300

    Cambiar url a producción

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,7 +1,7 @@
 import { GraphQLClient } from 'graphql-request'
-
+// 'http://localhost:4000/graphql'
 const endpoint = process.env.NODE_ENV === 'development' ?
-    'http://localhost:4000/graphql'
+    'https://materialusm.herokuapp.com/graphql'
     :
     'https://materialusm.herokuapp.com/graphql'
 
